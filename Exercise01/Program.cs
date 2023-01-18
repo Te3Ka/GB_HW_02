e5dc10582bbd5a724a56e91f9c89e9484d649adf
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-18 13:45:46 +0300

    Задание 1. Введение числа от пользователя

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -3,5 +3,14 @@
 /*Mnement4813@yandex.ru*/
 /***********************/
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+//Введение числа от пользователя
+Console.WriteLine("Программа показывает срединную (вторую) цифру введённого трёхзначного числа.");
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
